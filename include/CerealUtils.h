8549f6c261aa7759321ddba623acb7d1a2a26363
origamidance <origamidance@gmail.com>
2018-03-05 20:45:33 +0100

    updated cerealUtils

--- include/CerealUtils.h
+++ include/CerealUtils.h
@@ -8,6 +8,7 @@
 #endif //CINDERELLAAPP_CEREALUTILS_H
 #include <cereal/cereal.hpp>
 #include <Eigen/Core>
+#include <iostream>
 /**
  * @brief Serialisation of Eigen matrices for the serialisation
  * library cereal (http://uscilab.github.io/cereal/index.html).
@@ -23,13 +24,13 @@ namespace cereal {
  * @param ar[in] The Archive serialize to
  * @param m[in] The matrix to serialize
  */
-template <class Archive, class _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
-void save(Archive& ar, const ::Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>& m) {
+template<class Archive, class _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+void save(Archive &ar, const ::Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> &m) {
   int rows = m.rows(), cols = m.cols();
   ar(CEREAL_NVP(rows));
   ar(CEREAL_NVP(cols));
-  const std::vector<_Scalar> data(m.data(),m.data()+m.size());
-  ar(cereal::make_nvp("data",data));
+  const std::vector<_Scalar> data(m.data(), m.data() + m.size());
+  ar(cereal::make_nvp("data", data));
 }
 
 /**
@@ -37,15 +38,15 @@ void save(Archive& ar, const ::Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _M
  * @param ar
  * @param m
  */
-template <class Archive, class _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
-void load(Archive& ar, ::Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>& m) {
+template<class Archive, class _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+void load(Archive &ar, ::Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> &m) {
   int rows, cols;
   ar(CEREAL_NVP(rows));
   ar(CEREAL_NVP(cols));
   std::vector<_Scalar> data(rows*cols);
   ar(data);
-  if (rows * cols != m.size()) m.resize(rows, cols);
-  std::copy(data.begin(),data.end(),m.data());
+  if (rows*cols!=m.size()) m.resize(rows, cols);
+  std::copy(data.begin(), data.end(), m.data());
 }
 
 } /* namespace cereal */
