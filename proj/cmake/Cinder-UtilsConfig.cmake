c702b59c3cf86cec33494362c7c0b5d03b061775
origamidance <origamidance@gmail.com>
2018-03-09 17:54:03 +0100

    update

--- proj/cmake/Cinder-UtilsConfig.cmake
+++ proj/cmake/Cinder-UtilsConfig.cmake
@@ -1,9 +1,11 @@
 if (NOT TARGET Cinder-Utils)
+  get_filename_component(CINDER-UTILS_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
 find_package(Ceres REQUIRED)
 find_package(Eigen3 REQUIRED)
 set(Cinder-Utils_INCLUDES
   ${EIGEN3_INCLUDE_DIR}
   ${CERES_INCLUDE_DIRS}
+  ${CINDER-UTILS_PATH}/include
   )
 set(Cinder-Utils_LIBRARIES
   ${CERES_LIBRARIES}
